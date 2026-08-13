ff9cb56f6189db1b7586a127fc4541cd82e4395d
smartdevkenya-design <smartdevkenya@gmail.com>
2026-08-13 11:58:53 +0300

    Add ScrollToTop component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom'
 import Layout from './components/Layout'
+import ScrollToTop from './components/ScrollToTop'
 import Home from './pages/Home'
 import About from './pages/About'
 import Book from './pages/Book'
@@ -12,19 +13,22 @@ import NotFound from './pages/NotFound'
 
 function App() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="book" element={<Book />} />
-        <Route path="manifesto" element={<Manifesto />} />
-        <Route path="media" element={<Media />} />
-        <Route path="gallery" element={<Gallery />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="stories/:id" element={<Story />} />
-        <Route path="*" element={<NotFound />} />
-      </Route>
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="book" element={<Book />} />
+          <Route path="manifesto" element={<Manifesto />} />
+          <Route path="media" element={<Media />} />
+          <Route path="gallery" element={<Gallery />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="stories/:id" element={<Story />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </>
   )
 }
 
