df041bb569b180c91b3b483c6b1216417eb52ee0
smartdevkenya-design <smartdevkenya@gmail.com>
2026-08-15 08:48:06 +0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,42 @@
 import { Route, Routes } from 'react-router-dom'
 import Layout from './components/Layout'
-import ScrollToTop from './components/ScrollToTop'
+import ProtectedRoute from './components/ProtectedRoute'
 import Home from './pages/Home'
 import About from './pages/About'
 import Book from './pages/Book'
 import Manifesto from './pages/Manifesto'
 import Media from './pages/Media'
 import Gallery from './pages/Gallery'
-import Volunteer from './pages/Volunteer'
 import Contact from './pages/Contact'
 import Story from './pages/Story'
+import AdminLogin from './pages/AdminLogin'
+import Admin from './pages/Admin'
 import NotFound from './pages/NotFound'
 
 function App() {
   return (
-    <>
-      <ScrollToTop />
-      <Routes>
-        <Route element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="book" element={<Book />} />
-          <Route path="manifesto" element={<Manifesto />} />
-          <Route path="media" element={<Media />} />
-          <Route path="gallery" element={<Gallery />} />
-          <Route path="volunteer" element={<Volunteer />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="stories/:id" element={<Story />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="book" element={<Book />} />
+        <Route path="manifesto" element={<Manifesto />} />
+        <Route path="media" element={<Media />} />
+        <Route path="gallery" element={<Gallery />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="stories/:id" element={<Story />} />
+        <Route path="admin/login" element={<AdminLogin />} />
+        <Route
+          path="admin"
+          element={
+            <ProtectedRoute>
+              <Admin />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
+import { AuthProvider } from './lib/auth'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter basename="/ojwangmariam">
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
 )
