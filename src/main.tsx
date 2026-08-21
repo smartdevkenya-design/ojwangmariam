a0514d13c453462915bbf532e8d2391f966aba24
smartdevkenya-design <smartdevkenya@gmail.com>
2026-08-21 20:28:07 +0300

    Replace AuthProvider with SiteDataProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
-import { AuthProvider } from './lib/auth'
+import { SiteDataProvider } from './context/SiteDataContext'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter basename="/ojwangmariam">
-      <AuthProvider>
+      <SiteDataProvider>
         <App />
-      </AuthProvider>
+      </SiteDataProvider>
     </BrowserRouter>
   </StrictMode>,
 )
