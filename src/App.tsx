d1a0dfda672a43f8108cd6a1df807643b9636676
smartdevkenya-design <smartdevkenya@gmail.com>
2026-08-21 20:27:22 +0300

    Refactor routing with AuthProvider and new pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,72 @@
 import { Route, Routes } from 'react-router-dom'
 import Layout from './components/Layout'
-import ProtectedRoute from './components/ProtectedRoute'
+import ScrollToTop from './components/ScrollToTop'
 import Home from './pages/Home'
 import About from './pages/About'
 import Book from './pages/Book'
 import Manifesto from './pages/Manifesto'
 import Media from './pages/Media'
 import Gallery from './pages/Gallery'
+import Volunteer from './pages/Volunteer'
 import Contact from './pages/Contact'
 import Story from './pages/Story'
-import AdminLogin from './pages/AdminLogin'
-import Admin from './pages/Admin'
+import DynamicPage from './pages/DynamicPage'
 import NotFound from './pages/NotFound'
+
+import { AuthProvider } from './admin/AuthContext'
+import RequireAuth from './admin/RequireAuth'
+import AdminLayout from './admin/AdminLayout'
+import Login from './admin/Login'
+import Overview from './admin/Overview'
+import SiteSettingsPage from './admin/SiteSettingsPage'
+import ThemePage from './admin/ThemePage'
+import { PageEditorRoute } from './admin/PageEditorForm'
+import StoriesPage from './admin/StoriesPage'
+import GalleryImagesPage from './admin/GalleryImagesPage'
+import CustomPagesPage from './admin/CustomPagesPage'
 
 function App() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="book" element={<Book />} />
-        <Route path="manifesto" element={<Manifesto />} />
-        <Route path="media" element={<Media />} />
-        <Route path="gallery" element={<Gallery />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="stories/:id" element={<Story />} />
-        <Route path="admin/login" element={<AdminLogin />} />
+    <AuthProvider>
+      <ScrollToTop />
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="book" element={<Book />} />
+          <Route path="manifesto" element={<Manifesto />} />
+          <Route path="media" element={<Media />} />
+          <Route path="gallery" element={<Gallery />} />
+          <Route path="volunteer" element={<Volunteer />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="stories/:id" element={<Story />} />
+        </Route>
+
+        <Route path="admin/login" element={<Login />} />
         <Route
           path="admin"
           element={
-            <ProtectedRoute>
-              <Admin />
-            </ProtectedRoute>
+            <RequireAuth>
+              <AdminLayout />
+            </RequireAuth>
           }
-        />
-        <Route path="*" element={<NotFound />} />
-      </Route>
-    </Routes>
+        >
+          <Route index element={<Overview />} />
+          <Route path="settings" element={<SiteSettingsPage />} />
+          <Route path="theme" element={<ThemePage />} />
+          <Route path="pages/:page" element={<PageEditorRoute />} />
+          <Route path="stories" element={<StoriesPage />} />
+          <Route path="gallery-images" element={<GalleryImagesPage />} />
+          <Route path="custom-pages" element={<CustomPagesPage />} />
+        </Route>
+
+        {/* Admin-created extra pages live at the site root, e.g. /events */}
+        <Route element={<Layout />}>
+          <Route path=":slug" element={<DynamicPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </AuthProvider>
   )
 }
 
