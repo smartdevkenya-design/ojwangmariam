2038117e13f2592f28e2eb6a19bcd839466f5401
smartdevkenya-design <smartdevkenya@gmail.com>
2026-09-05 17:00:22 +0300

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/ojwangmariam/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
