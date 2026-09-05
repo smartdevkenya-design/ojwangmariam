c1e39fe976f27bf58f9994b57901b845d8ccdf8e
smartdevkenya-design <smartdevkenya@gmail.com>
2026-09-05 17:04:04 +0300

    Fix syntax for BrowserRouter component

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouterbasename="/">
+    <BrowserRouter basename="/">
       <SiteDataProvider>
         <App />
       </SiteDataProvider>
