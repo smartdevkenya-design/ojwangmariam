568f870513b08ce9c305c61030b2d941509dcddf
smartdevkenya-design <smartdevkenya@gmail.com>
2026-09-06 08:22:51 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,17 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import { SiteDataProvider } from './context/SiteDataContext'
+import { ErrorBoundary } from './ErrorBoundary'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/">
-      <SiteDataProvider>
-        <App />
-      </SiteDataProvider>
-    </BrowserRouter>
+    <ErrorBoundary>
+      <BrowserRouter basename="/">
+        <SiteDataProvider>
+          <App />
+        </SiteDataProvider>
+      </BrowserRouter>
+    </ErrorBoundary>
   </StrictMode>,
 )
